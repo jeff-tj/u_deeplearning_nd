bbd37798acdda41a978900592717cc549a1297de
jt <test@example.com>
2018-04-24 07:39:34 +1000

    Completed code for NN to pass requirements.

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -69,7 +69,7 @@ class NeuralNetwork(object):
 
         # TODO: Output layer - Replace these values with your calculations.
         final_inputs = np.matmul(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
-        final_outputs = self.activation_function(final_inputs) # signals from final output layer
+        final_outputs = final_inputs # signals from final output layer
 
         return final_outputs, hidden_outputs
 
@@ -88,20 +88,21 @@ class NeuralNetwork(object):
         ### Backward pass ###
 
         # TODO: Output error - Replace this value with your calculations.
+
         error = y - final_outputs # Output layer error is the difference between desired target and actual output.
 
         # TODO: Calculate the hidden layer's contribution to the error
         # hidden_error = None
 
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = error * final_outputs * (1-final_outputs)
+        output_error_term = error * 1.0 #* final_outputs * (1-final_outputs)
 
-        hidden_error = output_error_term * self.weights_hidden_to_output
+        hidden_error = np.matmul(self.weights_hidden_to_output, output_error_term) #JT Gives 1x2 matrix
 
-        hidden_error_term = hidden_error * np.dot(hidden_outputs, (1-hidden_outputs))
+        hidden_error_term = hidden_error * hidden_outputs * (1-hidden_outputs)
 
         # Weight step (input to hidden)
-        delta_weights_i_h += np.matmul(X[:, None], hidden_error_term.T)
+        delta_weights_i_h += np.matmul(X[:, None], hidden_error_term.reshape(1,self.hidden_nodes)) #Generalise
         # Weight step (hidden to output)
         delta_weights_h_o += output_error_term * hidden_outputs[:, None]
         return delta_weights_i_h, delta_weights_h_o
@@ -134,7 +135,7 @@ class NeuralNetwork(object):
 
         # TODO: Output layer - Replace these values with the appropriate calculations.
         final_inputs = np.matmul(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
-        final_outputs = self.activation_function(final_inputs) # signals from final output layer
+        final_outputs = final_inputs # signals from final output layer
 
         return final_outputs
 
